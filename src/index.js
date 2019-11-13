ade553993b53f406041a993f12955fa5e7742f70
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2019-11-13 23:37:14 +0300

    add from to get data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log(11111111111);
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+import App from "./App";
+
+console.log(reducer);
+const store = createStore(reducer);
+
+console.log(store);
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
